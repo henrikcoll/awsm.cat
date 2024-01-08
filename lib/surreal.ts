2ff983cd4780c2bba7dc2ba3ae91f1f5e72f9c92
Henrik Næss <henrik.karolus.naess@politiet.no>
2024-01-08 23:30:11 +0100

    feat: fix type error

--- lib/surreal.ts
+++ lib/surreal.ts
@@ -2,14 +2,15 @@ import Surreal from 'surrealdb.js';
 
 const surreal = new Surreal();
 
-surreal.connect(process.env.SURREAL_URL, {
-	namespace: process.env.SURREAL_NAMESPACE,
-	database: process.env.SURREAL_DATABASE,
+surreal.connect(process.env.SURREAL_URL as string, {
+	namespace: process.env.SURREAL_NAMESPACE ?? '',
+	database: process.env.SURREAL_DATABASE ?? '',
 	auth: {
-		namespace: process.env.SURREAL_NAMESPACE,
-		database: process.env.SURREAL_DATABASE,
-		username: process.env.SURREAL_USERNAME,
-		password: process.env.SURREAL_PASSWORD,
+		namespace: process.env.SURREAL_NAMESPACE ?? '',
+		database: process.env.SURREAL_DATABASE ?? '',
+		scope: process.env.SURREAL_SCOPE ?? '',
+		username: process.env.SURREAL_USERNAME ?? '',
+		password: process.env.SURREAL_PASSWORD ?? '',
 	},
 })
 
